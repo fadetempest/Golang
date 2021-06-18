2e2c973a8bae58b708487d6dec2ac07be90bb9cf
fadetempest <73659348+fadetempest@users.noreply.github.com>
2021-06-19 00:57:09 +0400

    Update fibonacci.go

--- fibonacci.go
+++ fibonacci.go
@@ -9,13 +9,13 @@ func main(){
 	fmt.Scan(&a)
 	fmt.Println("Введите второе число")
 	fmt.Scan(&b)
-	ar :=make([]int, b)
-	ar[0] = 0
-	ar[1] = 1
+	ar :=make([]int, b) //Create an empty array
+	ar[0] = 0 //First Fibonacci number
+	ar[1] = 1 //Second Fibonacci number
 	for i:=2; i < len(ar); i++{
-		ar[i] = ar[i-1] + ar[i-2]
+		ar[i] = ar[i-1] + ar[i-2] //Fill array with Fibonacci numbers
 	}
 	for i:=a - 1; i < b; i++{
-		fmt.Print(ar[i], " ")
+		fmt.Print(ar[i], " ") //Printing numbers from a to b
 	}
 }
